e538d87ed68faf8539ce5d0cd3cb096ef8953c45
Uki884 <mrchildreny@gmail.com>
2021-08-20 12:59:45 +0900

    ライブラリとして使えるようにした

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { App } from "vue";
+import InputProvider from "@/components/InputProvider.vue";
+export * from "@/compositions/useForm";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+const install = (vue: App) => {
+  vue.component(InputProvider.name, InputProvider);
+};
+
+export default { install };
